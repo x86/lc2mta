90aa0d23881ebb49d4a1c4cbc8126b26f9149242
MCvarial@gmail.com <MCvarial@gmail.com@8fe5d90a-bcbd-7d1c-a2ba-6198375b9fd7>
2012-08-22 11:31:35 +0000

    Added some useful default resources

---
+++ [gameplay]/reload/reload_c.lua
@@ -0,0 +1,9 @@
+﻿addCommandHandler("Reload weapon",
+	function()
+		local task = getPedSimplestTask(localPlayer)
+		if ((task == "TASK_SIMPLE_JUMP" or task == "TASK_SIMPLE_IN_AIR") and not doesPedHaveJetPack(localPlayer)) then return end
+		triggerServerEvent("onPlayerReload", localPlayer)
+	end
+)
+
+bindKey("r","down","Reload weapon")
